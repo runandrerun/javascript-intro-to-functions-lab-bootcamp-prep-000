c8735b74a6befa2c3faf435302f5df6921ad0dae
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-11 19:02:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,5 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return 'I love you, too';
-  } else {
-    return string;
-  }
+  } 
 }
