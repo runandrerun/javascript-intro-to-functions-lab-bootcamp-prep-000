e6db753cef06b2d2b2fdbeb4d600d140cc18dfa8
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-11 18:58:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,21 +7,20 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   } else if (string === string.toUpperCase()) {
-    return "YES INDEED!"
+    return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
-    return "I love you, too"
+    return "I love you, too";
   } else {
-    return "I love you, too"
   }
 }
